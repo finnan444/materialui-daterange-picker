9555b060fb36c1e576a74fef26046c04c2fe9525
Vladislav Rykovskiy <v.rykovskiy@zavod.games>
2021-05-28 14:56:06 +0300

    move components from package to src

--- package/src/index.ts
+++
@@ -1,8 +0,0 @@
-import DateRangePickerExporter from './components/DateRangePickerExporter';
-import { DateRange, DefinedRange } from './types';
-
-export {
-  DateRangePickerExporter as DateRangePicker,
-  DefinedRange,
-  DateRange,
-};
