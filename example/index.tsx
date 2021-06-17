b5a81c1ce78bbedd47661dd587b49aacc1057d6b
Vladislav Rykovskiy <v.rykovskiy@zavod.games>
2021-06-17 12:31:59 +0300

    add example

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,35 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import DateRangePickerExporter from '../src/components/DateRangePickerExporter';
+import { DateRange } from '../src';
 
 const App = () => {
+  const toggle = () => {};
+
+  const handleRangeSelect = (range: DateRange) => {
+    if (range) {
+      console.log(range);
+    }
+  };
+
+  const minDate = new Date();
+  minDate.setDate(minDate.getDate() - 7);
+
   return (
     <div>
-      <Thing />
+      <DateRangePickerExporter
+        open
+        closeOnClickOutside={false}
+        toggle={toggle}
+        onChange={handleRangeSelect}
+        // initialDateRange={defaultSelected}
+        // locale={enGB}
+        popoverProps={{
+          elevation: 0,
+        }}
+        minDate={minDate}
+      />
     </div>
   );
 };
